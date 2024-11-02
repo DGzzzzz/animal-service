fa4cda8d96045ba013c8ae2590fdeefba6fe10ff
douglaswinter75@gmail.com <douglaswinter@outlook.com>
2024-11-02 11:50:18 -0300

    :recycle: refactor: alter method qtdAnimalPorRecebedor

--- src/main/java/com/dg/animal_service/controllers/AnimalController.java
+++ src/main/java/com/dg/animal_service/controllers/AnimalController.java
@@ -3,9 +3,11 @@ package com.dg.animal_service.controllers;
 import com.dg.animal_service.entidades.Animal;
 import com.dg.animal_service.projections.RecebedorQtd;
 import com.dg.animal_service.repositories.AnimalRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 @RestController
@@ -33,10 +35,17 @@ public class AnimalController {
         return repository.findByAdopter();
     }
 
-    @GetMapping("/recebedor")
-    private List<RecebedorQtd> qtdAnimalPorRecebedor() {
-        LocalDate dataFim = LocalDate.now();
-        LocalDate dataInicio = dataFim.minusYears(1);
-        return repository.qtdAnimalPorRecebedor(dataInicio, dataFim);
+    @GetMapping("/resgates")
+    private ResponseEntity<?> qtdAnimalPorRecebedor(
+            @RequestParam LocalDate dataInicio,
+            @RequestParam LocalDate dataFim
+    ) {
+
+        if (Period.between(dataInicio, dataFim).getYears() >= 1) {
+            return ResponseEntity.badRequest().body("O intervalo máximo entre as datas é de um ano");
+        }
+
+        List<RecebedorQtd> resultado = repository.qtdAnimalPorRecebedor(dataInicio, dataFim);
+        return ResponseEntity.ok(resultado);
     }
 }
